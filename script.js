4e61702828fe4950ac7961d281d199a88b5333de
marshmellow89 <pritamsingh100app@gmail.com>
2025-07-26 15:54:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-//your JS code here. If required.
+// //your JS code here. If required.
 
- const student = {
-	name: "john",
-	age: "20",
-	city: "delhi"
-}
+//  const student = {
+// 	name: "john",
+// 	age: "20",
+// 	city: "delhi"
+// }
 
-function getKeys(obj){
-	return Object.keys(obj);
+// function getKeys(obj){
+// 	return Object.keys(obj);
+// }
+
+
+Object.prototype.getKeys = function () {
+  return Object.keys(this);
 }
+ 
+// Create student object
+const student = {
+  name: "John"
+};
